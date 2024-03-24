8714a703ec7b0894203515a87b6184b3f46a82d9
Gopika <122785613+Gopika-1383@users.noreply.github.com>
2024-03-24 13:32:54 +0530

    Create MySQLConnector.py

---
+++ MySQLConnector.py
@@ -0,0 +1,7 @@
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="XXXXXXXXXi@1",database="mansahaidatabase")
+mycursor=mydb.cursor()
+mycursor.execute("select * from mansahai")
+d=mycursor.fetchall()
+for i in d:
+    print(i)
